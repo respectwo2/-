eb79b477434567c65264395b6232a42792e237af
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-07-06 17:45:15 +0900

    0706 머지 완료

--- demo/src/main/java/com/example/jinyengandothers/cotroller/HomeController.java
+++ demo/src/main/java/com/example/jinyengandothers/cotroller/HomeController.java
@@ -3,17 +3,23 @@ package com.example.jinyengandothers.cotroller;
 import java.util.List;
 
 import org.apache.catalina.connector.Response;
+import org.mybatis.logging.Logger;
+import org.mybatis.logging.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.jinyengandothers.dao.CoinPriceDao;
 import com.example.jinyengandothers.dto.NewsDto;
+import com.example.jinyengandothers.service.BackTestingSample;
+import com.example.jinyengandothers.service.BackTestingSample2;
 import com.example.jinyengandothers.service.CryptoCompareNewsService;
 
 
